169a8d5f29f14ddffa2a586dea231e4beefe00a6
Aryazaky <aryazakyazif@gmail.com>
2025-01-24 17:05:02 +0700

    stat comment

--- Assets/Scripts/Stat.cs
+++ Assets/Scripts/Stat.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+/// <summary>
+/// An immutable stat with a type, value, and optional min/max range, supporting precision handling.
+/// </summary>
 public readonly partial struct Stat : Stat.IStat
 {
     private readonly int _precision;
@@ -12,6 +15,21 @@ public readonly partial struct Stat : Stat.IStat
 
     public float? Max { get; }
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="Stat"/>.
+    /// The constructor ensures that the provided value is within the specified range (min to max, if not null),
+    /// and rounds the value to the given precision. 
+    /// </summary>
+    /// <remarks>
+    /// If the precision is negative, an exception is thrown.
+    /// If the max value is less than the min value, an exception is thrown.
+    /// </remarks>
+    /// <param name="type">The type of the stat (e.g., health, stamina, etc.).</param>
+    /// <param name="value">The value of the stat, which will be clamped between the min and max values.</param>
+    /// <param name="min">The minimum allowable value for the stat (optional, defaults to 0).</param>
+    /// <param name="max">The maximum allowable value for the stat (optional, defaults to null, which means no limit).</param>
+    /// <param name="precision">The number of decimal places to round the value to (optional, defaults to 0).</param>
+    /// <exception cref="ArgumentException">Thrown if precision is negative, or if max is less than min.</exception>
     public Stat(StatType type, float value, float? min = 0, float? max = null, int precision = 0)
     {
         if (precision < 0)
@@ -32,12 +50,16 @@ public readonly partial struct Stat : Stat.IStat
         value = Min.HasValue ? MathF.Max(Min.Value, value) : value;
         Value = MathF.Round(value, _precision);
     }
-
+    
+    /// <param name="delta">The value to add to existing value</param>
+    /// <returns>A new Stat instance with the value adjusted by a given delta.</returns>
     public Stat UpdateValue(float delta)
     {
         return new Stat(Type, Value + delta, Min, Max, _precision);
     }
 
+    /// <param name="value">The value to replace the existing value</param>
+    /// <returns>A new Stat instance with the replaced value.</returns>
     public Stat SetValue(float value)
     {
         return new Stat(Type, value, Min, Max, _precision);
